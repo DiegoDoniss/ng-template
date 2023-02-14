2e062023270789328f7544039b741bc56be3b2cf
diego donis <diego.jdonis@gmail.com>
2023-02-14 09:58:55 -0600

    structure of layout with example module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent,
+    MainComponent
+  ],
   imports: [
     CommonModule,
     LayoutRoutingModule
