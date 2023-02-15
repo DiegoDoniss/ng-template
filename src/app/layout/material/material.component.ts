fb7fdfc0a04402deb653afc57d9535fa333f7a8b
diego donis <diego.jdonis@gmail.com>
2023-02-15 16:48:11 -0600

    add example routes

--- src/app/layout/material/material.component.ts
+++ src/app/layout/material/material.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+
+const navigationRoutes = [
+  {
+    module: 'example', routes: [
+      {title:'first',uri:''},
+      {title:'second',uri:''},
+      {title:'example',uri:''},
+    ]
+  }
+]
 
 @Component({
   selector: 'app-material',
@@ -9,13 +19,14 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./material.component.scss']
 })
 export class MaterialComponent {
-
+  navigation = navigationRoutes
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
 }
